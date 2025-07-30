97210071946bc983cb47f3caf82d86a9f24afb8b
Raymond Gigliotti <ray@firestitch.com>
2025-07-30 07:20:06 -0400

    export additional interfaces in public API

--- src/public_api.ts
+++ src/public_api.ts
@@ -2,5 +2,6 @@ export { FsActivitiesComponent } from './app/components/activities/activities.co
 export { FsActivityObjectTypeComponent } from './app/components/activity-object-type/activity-object-type.component';
 export * from './app/components/activity-previews';
 export { FsActivityPreviewDirective } from './app/directives/activity-preview.directive';
+export * from './app/interfaces';
 export { ActivityConfig } from './app/interfaces/activity-config';
 
